bc8c6bb4c98a5779ab529068b347937534e6616f
isaac-w18 <isaacw1218@gmail.com>
2023-04-14 17:40:22 -0400

    Books sheet cancel button

--- Bibley/Bibley/Views/BooksView.swift
+++ Bibley/Bibley/Views/BooksView.swift
@@ -8,20 +8,43 @@
 import SwiftUI
 
 struct BooksView: View {
+    @Environment(\.dismiss) var dismiss
     @Binding var book: Book
     @Binding var chapterNum: Int
     
     var body: some View {
-        ForEach(Book.allCases, id: \.self) {b in
-            Text(b.getFormattedBook())
-        }
+            List {
+                Section {
+                    ForEach(Book.allCases, id: \.self) {b in
+                        Text(b.getFormattedBook())
+                    }
+                } header: {
+                    HStack {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text("Cancel")
+                        }
+                        .buttonStyle(.plain)
+                        Spacer()
+                        Text("Books").bold()
+                        Spacer()
+                        Text("History")
+                    }
+                }
+            }
+            
+        
     }
 }
 
 
 
 //struct BooksView_Previews: PreviewProvider {
+//    @State private var book = Book.John
+//    @State private var chapterNum = 1
+//
 //    static var previews: some View {
-//        BooksView()
+//        BooksView(book: $book, chapterNum: $chapterNum)
 //    }
 //}
